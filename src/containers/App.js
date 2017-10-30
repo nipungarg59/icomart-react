7b6df06a760cec7dd2779b75e0e25fc41b563659
Ujjwal Sharma <usharma1998@gmail.com>
2017-10-31 03:32:14 +0530

    Add bootstrap support

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,14 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Link
+// } from 'react-router-dom'
+
+import Navbar from '../components/Navbar'
 
 const App = () => (
-  <Router>
-    <Route path="/auth/login" component={Login}/>
-    <Route path="/auth/register" component={Register}/>
-    <Route path="/auth/reset/:token" component={Reset}/>
-    <Route path="/auth/confirm/:token" component={Confirm}/>
-  </Router>
+  <Navbar/>
 )
 
 export default App
