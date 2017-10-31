e880a05bae82a37aaf7ba34740f1e882af497640
Ujjwal Sharma <usharma1998@gmail.com>
2017-11-01 01:33:37 +0530

    Convert Register to a classical component

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {
   Form,
   FormGroup,
@@ -6,40 +6,44 @@ import {
   FormControl
 } from 'react-bootstrap'
 
-const Register = () => (
-  <Form horizontal>
-    <FormGroup controlId="registerFirstName">
-      <ControlLabel>First Name</ControlLabel>
-      <FormControl type="text" placeholder="John"/>
-    </FormGroup>
-    <FormGroup controlId="registerLastName">
-      <ControlLabel>Last Name</ControlLabel>
-      <FormControl type="text" placeholder="Doe"/>
-    </FormGroup>
-    <FormGroup controlId="registerDateOfBirth">
-      <ControlLabel>Date of Birth</ControlLabel>
-      <FormControl type="date"/>
-    </FormGroup>
-    <FormGroup controlId="registerEmail">
-      <ControlLabel>Email</ControlLabel>
-      <FormControl type="email" placeholder="john.doe@example.com"/>
-    </FormGroup>
-    <FormGroup controlId="registerPhone">
-      <ControlLabel>Phone Number</ControlLabel>
-      <FormControl type="number"/>
-    </FormGroup>
-    <FormGroup controlId="registerUsername">
-      <ControlLabel>Username</ControlLabel>
-      <FormControl type="text" placeholder="john.doe"/>
-    </FormGroup>
-    <FormGroup controlId="registerPassword">
-      <ControlLabel>Password</ControlLabel>
-      <FormControl type="password"/>
-    </FormGroup>
-    <FormGroup controlId="registerSubmit">
-      <FormControl type="submit" value="Register"/>
-    </FormGroup>
-  </Form>
-)
+class Register extends Component {
+  render() {
+    return (
+      <Form horizontal>
+        <FormGroup controlId="registerFirstName">
+          <ControlLabel>First Name</ControlLabel>
+          <FormControl type="text" placeholder="John"/>
+        </FormGroup>
+        <FormGroup controlId="registerLastName">
+          <ControlLabel>Last Name</ControlLabel>
+          <FormControl type="text" placeholder="Doe"/>
+        </FormGroup>
+        <FormGroup controlId="registerDateOfBirth">
+          <ControlLabel>Date of Birth</ControlLabel>
+          <FormControl type="date"/>
+        </FormGroup>
+        <FormGroup controlId="registerEmail">
+          <ControlLabel>Email</ControlLabel>
+          <FormControl type="email" placeholder="john.doe@example.com"/>
+        </FormGroup>
+        <FormGroup controlId="registerPhone">
+          <ControlLabel>Phone Number</ControlLabel>
+          <FormControl type="number"/>
+        </FormGroup>
+        <FormGroup controlId="registerUsername">
+          <ControlLabel>Username</ControlLabel>
+          <FormControl type="text" placeholder="john.doe"/>
+        </FormGroup>
+        <FormGroup controlId="registerPassword">
+          <ControlLabel>Password</ControlLabel>
+          <FormControl type="password"/>
+        </FormGroup>
+        <FormGroup controlId="registerSubmit">
+          <FormControl type="submit" value="Register"/>
+        </FormGroup>
+      </Form>
+    )
+  }
+}
 
 export default Register
