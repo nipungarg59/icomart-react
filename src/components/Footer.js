97556cf92c1b8d1c78f2a921844ee0f9b6e37fac
Ujjwal Sharma <usharma1998@gmail.com>
2017-12-02 17:28:38 +0530

    Use aphrodite in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import { StyleSheet, css } from 'aphrodite'
 import { Grid, Row, Col } from 'react-bootstrap'
 
-const styles = {
+const styles = StyleSheet.create({
   footer: {
     background: '#16222a',
     background: 'linear-gradient(to right, #16222a, #3a6073)',
@@ -19,49 +20,49 @@ const styles = {
     height: '100px',
     border: '1px solid white'
   }
-}
+})
 
 const Footer = () => (
-  <div style={styles.footer}>
+  <div className={css(styles.footer)}>
     <Grid>
       <Row>
         <Col md={6}>
           <Row>
             <Col sm={6}>
-              <img src="" alt="LOGO" style={styles.logo}/>
+              <img className={css(styles.logo)} src="" alt="LOGO"/>
               <p>Punchline</p>
               <p>© 2017 All rights reserved</p>
             </Col>
             <Col sm={6}>
-              <ul style={styles.list}>
-                <li><a href="" style={styles.link}>Terms</a></li>
-                <li><a href="" style={styles.link}>Privacy</a></li>
+              <ul className={css(styles.list)}>
+                <li><a href="" className={css(styles.link)}>Terms</a></li>
+                <li><a href="" className={css(styles.link)}>Privacy</a></li>
               </ul>
             </Col>
           </Row>
         </Col>
         <Col md={2}>
-          <ul style={styles.list}>
-            <li><a href="" style={styles.link}>other</a></li>
-            <li><a href="" style={styles.link}>links</a></li>
-            <li><a href="" style={styles.link}>as</a></li>
-            <li><a href="" style={styles.link}>needed</a></li>
+          <ul className={css(styles.list)}>
+            <li><a href="" className={css(styles.link)}>other</a></li>
+            <li><a href="" className={css(styles.link)}>links</a></li>
+            <li><a href="" className={css(styles.link)}>as</a></li>
+            <li><a href="" className={css(styles.link)}>needed</a></li>
           </ul>
         </Col>
         <Col md={2}>
-          <ul style={styles.list}>
-            <li><a href="" style={styles.link}>Market Insight</a></li>
-            <li><a href="" style={styles.link}>ICO List</a></li>
-            <li><a href="" style={styles.link}>ICO Stats</a></li>
-            <li><a href="" style={styles.link}>Rating System</a></li>
+          <ul className={css(styles.list)}>
+            <li><a href="" className={css(styles.link)}>Market Insight</a></li>
+            <li><a href="" className={css(styles.link)}>ICO List</a></li>
+            <li><a href="" className={css(styles.link)}>ICO Stats</a></li>
+            <li><a href="" className={css(styles.link)}>Rating System</a></li>
           </ul>
         </Col>
         <Col md={2}>
-          <ul style={styles.list}>
-            <li><a href="" style={styles.link}>Social</a></li>
-            <li><a href="" style={styles.link}>Facebook</a></li>
-            <li><a href="" style={styles.link}>Twitter</a></li>
-            <li><a href="" style={styles.link}>Telegram</a></li>
+          <ul className={css(styles.list)}>
+            <li><a href="" className={css(styles.link)}>Social</a></li>
+            <li><a href="" className={css(styles.link)}>Facebook</a></li>
+            <li><a href="" className={css(styles.link)}>Twitter</a></li>
+            <li><a href="" className={css(styles.link)}>Telegram</a></li>
           </ul>
         </Col>
       </Row>
