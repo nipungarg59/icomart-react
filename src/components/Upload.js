012c16f95cd79d724e426b359cef92b6bd435437
Ujjwal Sharma <usharma1998@gmail.com>
2017-12-18 15:05:09 +0530

    Prettify Upload.js

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,30 +1,31 @@
-import React from 'react'
-import Dropzone from 'react-dropzone'
+import React from "react";
+import Dropzone from "react-dropzone";
 
 const onDrop = (action, file) => {
-  let data = new FormData()
-  data.append('file', file, file.name)
-  data.append('bucket', 'ico_list')
+  let data = new FormData();
+  data.append("file", file, file.name);
+  data.append("bucket", "ico_list");
 
   fetch(`http://api.staging.icofarm.net/upload/file`, {
     method: "POST",
-    body: data,
-  }).then(res => res.json()).then(res => {
-    action(res.url)
+    body: data
   })
-}
+    .then(res => res.json())
+    .then(res => {
+      action(res.url);
+    });
+};
 
 const Upload = ({ action, url }) => (
   <div className="dropzone">
     <Dropzone onDrop={files => onDrop(action, files[0])}>
-      {
-        url ?
-          <img src={url} style={{'width': '200px', 'height' : '200px'}}/>
-        :
+      {url ? (
+        <img src={url} style={{ width: "200px", height: "200px" }} />
+      ) : (
         <p>Try dropping some files here, or click to select files to upload.</p>
-      }
+      )}
     </Dropzone>
   </div>
-)
+);
 
-export default Upload
+export default Upload;
