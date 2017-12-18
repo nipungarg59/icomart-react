6b99d586b4f3e2dce82557577e94e0f94f16a3ee
Ujjwal Sharma <usharma1998@gmail.com>
2017-12-18 15:08:44 +0530

    Prettify Dashboard.js

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react'
-import {Tabs, Tab, Grid } from 'react-bootstrap'
+import React, { Component } from "react";
+import { Tabs, Tab, Grid } from "react-bootstrap";
 
-import IcoList from '../components/IcoList'
-import Team from '../components/Team'
-import Main from '../components/Main'
+import IcoList from "../components/IcoList";
+import Team from "../components/Team";
+import Main from "../components/Main";
 
-import './Dashboard.css'
+import "./Dashboard.css";
 
 class Dashboard extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       trending: [],
       upcoming: [],
       ongoing: [],
       recent: [],
-      team : [],
+      team: [],
       tab: 3
-    }
+    };
 
-    this.handleTab = this.handleTab.bind(this)
-    this.getTeamData = this.getTeamData.bind(this)
-    this.onSetTab = this.onSetTab.bind(this)
+    this.handleTab = this.handleTab.bind(this);
+    this.getTeamData = this.getTeamData.bind(this);
+    this.onSetTab = this.onSetTab.bind(this);
   }
 
   sendRequest(filter) {
@@ -31,63 +31,67 @@ class Dashboard extends Component {
       body: JSON.stringify({ filter }),
       headers: {
         "Content-Type": "application/json"
-      },
-    }).then(res => res.json()).then(res => {
-      switch(filter) {
-        case 'TRENDING':
-          this.setState({ trending: res.list })
-          break
-        case 'UPCOMING':
-          this.setState({ upcoming: res.list })
-          break
-        case 'ONGOING':
-          this.setState({ ongoing: res.list })
-          break
-        case 'RECENT':
-          this.setState({ recent: res.list })
-          break
       }
     })
+      .then(res => res.json())
+      .then(res => {
+        switch (filter) {
+          case "TRENDING":
+            this.setState({ trending: res.list });
+            break;
+          case "UPCOMING":
+            this.setState({ upcoming: res.list });
+            break;
+          case "ONGOING":
+            this.setState({ ongoing: res.list });
+            break;
+          case "RECENT":
+            this.setState({ recent: res.list });
+            break;
+        }
+      });
   }
 
   handleTab(tab) {
-    switch(tab) {
+    switch (tab) {
       case 1:
-        this.sendRequest('TRENDING')
-        break
+        this.sendRequest("TRENDING");
+        break;
       case 2:
-        this.sendRequest('UPCOMING')
-        break
+        this.sendRequest("UPCOMING");
+        break;
       case 3:
-        this.sendRequest('ONGOING')
-        break
+        this.sendRequest("ONGOING");
+        break;
       case 4:
-        this.sendRequest('RECENT')
-        break
+        this.sendRequest("RECENT");
+        break;
     }
   }
 
-  getTeamData(){
-    fetch(`http://${this.props.baseURL}/admin/get/team/members`).then(res => res.json()).then(res => {
-      console.log(res)
-      this.setState({team : res.members})
-    })
+  getTeamData() {
+    fetch(`http://${this.props.baseURL}/admin/get/team/members`)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        this.setState({ team: res.members });
+      });
   }
 
   componentDidMount() {
-    this.handleTab(this.state.tab)
-    this.getTeamData()
+    this.handleTab(this.state.tab);
+    this.getTeamData();
   }
 
   onSetTab(tab) {
-    this.setState({ tab })
-    this.handleTab(tab)
+    this.setState({ tab });
+    this.handleTab(tab);
   }
 
   render() {
     return (
       <div>
-        <Main onSelectTab={this.onSetTab}/>
+        <Main onSelectTab={this.onSetTab} />
         <Grid>
           <Tab.Container id="tabs-ico" activeKey={this.state.tab}>
             <Tab.Content animation>
@@ -101,17 +105,17 @@ class Dashboard extends Component {
                 <IcoList title="Ongoing" data={this.state.ongoing} />
               </Tab.Pane>
               <Tab.Pane eventKey={4}>
-                <IcoList title="Recent" data={this.state.recent}/>
+                <IcoList title="Recent" data={this.state.recent} />
               </Tab.Pane>
             </Tab.Content>
           </Tab.Container>
         </Grid>
-        <div >
-          <Team data={this.state.team}/>
+        <div>
+          <Team data={this.state.team} />
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Dashboard
+export default Dashboard;
